270229c83e0763b9c8c2c471486272507405992c
ADITYA SHETTY <161926174+ShettyAditya24@users.noreply.github.com>
2025-06-17 18:00:28 +0530

    Add files via upload

---
+++ contro.py
@@ -0,0 +1,9 @@
+
+n=int(input("Enter a number:"))
+
+if(n%2==0):
+    print(n,"is an even number.")
+elif(n<0):
+    print(n,"is an invalid input.")
+else:
+    print(n,"is an odd number.")
---
+++ task3.py
@@ -0,0 +1,6 @@
+total_sum=0
+
+for n in range(1,51):
+    total_sum=total_sum+n
+
+print("The sum of numbers from 1 to 50 is:",total_sum)
